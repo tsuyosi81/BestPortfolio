315daac360567c90fb89611339c8bdf2e7ed17de
tsuyosi81 <tharayama@icstars.org>
2022-03-25 19:02:25 -0500

    toggler menu function

--- main.js
+++ main.js
@@ -18,64 +18,64 @@
 })(jQuery);
 
 // NOTE Project Filter
-$(function () {
-    var $grid = $('#tp-grid'),
-        $name = $('#name'),
-        $close = $('#close'),
-        $loader = $('').insertBefore($grid),
-        stapel = $grid.stapel({
-            randomAngle: true,
-            delay: 50,
-            gutter: 70,
-            pileAngles: 5,
-            onLoad: function () {
-                $loader.remove();
-            },
-            onBeforeOpen: function (pileName) {
-                $name.html(pileName);
-            },
-            onAfterOpen: function (pileName) {
-                $close.show();
-            }
-        });
-    $close.on('click', function () {
-        $close.hide();
-        $name.empty();
-        stapel.closePile();
-    });
+// $(function () {
+//     var $grid = $('#tp-grid'),
+//         $name = $('#name'),
+//         $close = $('#close'),
+//         $loader = $('').insertBefore($grid),
+//         stapel = $grid.stapel({
+//             randomAngle: true,
+//             delay: 50,
+//             gutter: 70,
+//             pileAngles: 5,
+//             onLoad: function () {
+//                 $loader.remove();
+//             },
+//             onBeforeOpen: function (pileName) {
+//                 $name.html(pileName);
+//             },
+//             onAfterOpen: function (pileName) {
+//                 $close.show();
+//             }
+//         });
+//     $close.on('click', function () {
+//         $close.hide();
+//         $name.empty();
+//         stapel.closePile();
+//     });
 
 
-    jQuery(function ($) {
-        "use strict";
-        $('#flat').cubeportfolio({
-            filters: '#flat',
-            layoutMode: 'mosaic',
-            sortByDimension: true,
-            mediaQueries: [{
-                width: 1300,
-                cols: 6,
-            }, {
-                width: 1100,
-                cols: 6,
-            }, {
-                width: 920,
-                cols: 6,
-            }, {
-                width: 400,
-                cols: 1,
-                options: {
-                    gapHorizontal: 15,
-                    gapVertical: 15,
-                }
-            }],
-            defaultFilter: '*',
-            animationType: 'fadeOutTop',
-            gapHorizontal: 0,
-            gapVertical: 0,
-            gridAdjustment: 'responsive',
-            caption: 'zoom',
-            lightboxDelegate: '.cbp-lightbox',
-            lightboxGallery: true,
-            lightboxTitleSrc: 'data-title',
-        });
-    });
+//     jQuery(function ($) {
+//         "use strict";
+//         $('#flat').cubeportfolio({
+//             filters: '#flat',
+//             layoutMode: 'mosaic',
+//             sortByDimension: true,
+//             mediaQueries: [{
+//                 width: 1300,
+//                 cols: 6,
+//             }, {
+//                 width: 1100,
+//                 cols: 6,
+//             }, {
+//                 width: 920,
+//                 cols: 6,
+//             }, {
+//                 width: 400,
+//                 cols: 1,
+//                 options: {
+//                     gapHorizontal: 15,
+//                     gapVertical: 15,
+//                 }
+//             }],
+//             defaultFilter: '*',
+//             animationType: 'fadeOutTop',
+//             gapHorizontal: 0,
+//             gapVertical: 0,
+//             gridAdjustment: 'responsive',
+//             caption: 'zoom',
+//             lightboxDelegate: '.cbp-lightbox',
+//             lightboxGallery: true,
+//             lightboxTitleSrc: 'data-title',
+//         });
+//     });
