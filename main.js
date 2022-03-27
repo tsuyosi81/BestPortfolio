e72e8c9a10bf663fc6ca4cbd7a845f888a677592
tsuyosi81 <tharayama@icstars.org>
2022-03-27 15:37:12 -0500

    project donw

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
 // NOTE Toggler Menu
-// (function ($) {
-//     "use strict";
-//     var NAY = {};
-//     NAY.MenuTogglerClose = function () {
-//         $(".toggler-menu").on('click', function () {
-//             $('.header-left').stop().toggleClass('menu-open');
-//         });
 
-//         $('.header-left a').on('click', function () {
-//             var toggle = $('.toggler-menu');
-//         });
-//     }
+(function ($) {
+    "use strict";
+    var NAY = {};
+    NAY.MenuTogglerClose = function () {
+        $(".toggler-menu").on('click', function () {
+            $('.header-left').stop().toggleClass('menu-open');
+        });
 
-//     $(document).on("ready", function () {
-//         NAY.MenuTogglerClose()
-//     });
-// })(jQuery);
+        $('.header-left a').on('click', function () {
+            var toggle = $('.toggler-menu');
+        });
+    }
+
+    $(document).on("ready", function () {
+        NAY.MenuTogglerClose()
+    });
+})(jQuery);
 
 // NOTE Project Filter
 
-// For-filter-menu
+// For - filter - menu
 
-// $(document).on('click', '.project-filter li', function () {
-//     $(this).addClass('project-filter-active').siblings().removeClass('project-filter-active')
-// });
+$(document).on('click', '.project-filter li', function () {
+    $(this).addClass('project-filter-active').siblings().removeClass('project-filter-active')
+});
 
-// // For-Project/work-filter
+// For-Project/work-filter
 
-// $(document).ready(function () {
-//     $('.list').click(function () {
-//         const value = $(this).attr('data-filter');
-//         if (value == 'all') {
-//             $('project-box').show('1000');
-//         } else {
-//             $('.project-box').not('.' + value).hide('1000');
-//             $('.project-box').filter('.' + value).show('1000');
-//         }
-//     })
-// })
+$(document).ready(function () {
+    $('.list').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('project-box').show('1000');
+        } else {
+            $('.project-box').not('.' + value).hide('1000');
+            $('.project-box').filter('.' + value).show('1000');
+        }
+    })
+})
 
 // NOTE Scroll Fade in
 
@@ -60,3 +61,4 @@ function reveal() {
         };
     };
 };
+
